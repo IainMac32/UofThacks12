6619a12e707dc1dbcd659befdb7780693a5aa8b8
Hayden <haydenc9898@gmail.com>
2025-01-18 20:05:25 -0500

    Added related questions table

--- frontend/src/option.js
+++ frontend/src/option.js
@@ -14,6 +14,15 @@ function App(){
     const handleClick = () => {
         navigate("/slideshow");
       }
+    //Use API to replace this data
+    const data=[
+        {id:"1", question:"Question #1", link:"https://example.com"},
+        {id:"2", question:"Question #2", link:"https://example.com"},
+        {id:"3", question:"Question #3", link:"https://example.com"},
+        {id:"4", question:"Question #4", link:"https://example.com"},
+        {id:"5", question:"Question #5", link:"https://example.com"},
+        {id:"6", question:"Question #6", link:"https://example.com"},
+    ];
     return(
         <div className='option-page'>
             <div className="option-title">
@@ -35,8 +44,23 @@ function App(){
                 onClick={handleClick}
                 disabled={!text} 
                 >
-                    <FaCircleArrowRight />
+                <FaCircleArrowRight />
                 </button>
+            </div>
+            <p className="similar-results-label"> Popular Questions</p>
+            <div className="similar-results-table">
+                {data.map((item) =>
+                (<div className="table-row" key={item.id} >
+                    <p>{item.id}.</p>
+                    <a href={item.link}
+                    target="_blank"
+                    rel ="noopener noreferrer"
+                    className="question-link"
+                    >
+                        {item.question}
+                    </a>
+                </div>
+                ))}
             </div>
             </div>
         </div>
