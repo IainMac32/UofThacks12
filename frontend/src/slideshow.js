cfd314e3130a464c138e98450f287c12f3535ef0
Yadon Kassahun <yekassah@gmail.com>
2025-01-18 22:44:27 -0500

    UI fixed + 1

--- frontend/src/slideshow.js
+++ frontend/src/slideshow.js
@@ -180,40 +180,8 @@ const handleSendClick = () => {
         <button className="back-button" onClick={handleBack}>Back</button>
         <button className="nav-button" onClick={handleNext}>→</button>
       </div>
-      
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-function App (){
-    const location = useLocation();
-    const query = location.state?.query || "No query provided";
-    useEffect(() => {
-        if (query && query !== 'No query provided') {
-          // Define the payload for the POST request
-          const payload = { userQuery: query };
-            console.log("Break-----------------------------");
-          // Perform the POST request
-          fetch('https://flask-app-i7sgeivnqa-uc.a.run.app/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('Success:', data);
-            })
-            .catch((error) => {
-              console.error('Error:', error);
-            });
-        }
-      }, [query]);
-    
-    return(
-    <div>
-        <center><h1>Slideshow Page</h1></center>
     </div>
     );
 }
 
-export default App; 
+export default App;
