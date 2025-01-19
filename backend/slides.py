8944f6a9b6a496c27a51fd6e17120151ea447fae
IainMac32 <iainhmacdonald@gmail.com>
2025-01-18 19:29:33 -0500

    change location of cred

--- backend/slides.py
+++ backend/slides.py
@@ -5,23 +5,22 @@ import webbrowser
 from pyspark.sql import SparkSession
 
 
+# Path to your service account key file
+SERVICE_ACCOUNT_FILE = "./credentials.json"
+
+# Authenticate using the service account
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=["https://www.googleapis.com/auth/presentations", "https://www.googleapis.com/auth/drive"]
+)
+
+# Build the Slides API service
+slides_service = build("slides", "v1", credentials=credentials)
+drive_service = build("drive", "v3", credentials=credentials)
+
+
+
 def initializePresentation(title,topic,user_topic):
-
-    # Path to your service account key file
-    SERVICE_ACCOUNT_FILE = "./credentials.json"
-
-    # Authenticate using the service account
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
-        scopes=["https://www.googleapis.com/auth/presentations", "https://www.googleapis.com/auth/drive"]
-    )
-
-    # Build the Slides API service
-    slides_service = build("slides", "v1", credentials=credentials)
-    drive_service = build("drive", "v3", credentials=credentials)
-
-
-
     # Create a new blank presentation
     presentation = slides_service.presentations().create(body={"title": title}).execute()
 
@@ -315,8 +314,4 @@ def create_slideshow(json,topic):
         "images": ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/1200px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg","https://www.varsitytutors.com/images/earlyamerica/washington.jpg"],
         "descs": [";laskdj;lkasdjf;lkasdjfl;askdjf","Slides about George. Slides about George. Slides about George. Slides about George. Slides about George. Slides about George. "]
     }
-
     handleData(json,presentation_id,topic)
-
-
-create_slideshow(1,3)
